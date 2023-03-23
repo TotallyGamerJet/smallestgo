5d253f3f5b9bcd286d5d01e1e25fd43106ae35fa
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-03-23 14:58:16 -0400

    remove

--- src/traceback.go
+++ src/traceback.go
@@ -2,38 +2,8 @@ package runtime
 
 import "unsafe"
 
-const usesLR = false
-
 func callers(skip int, pcbuf []uintptr) int {
 	return 0
-}
-
-func isSystemGoroutine(gp *g, fixed bool) bool {
-	return false
-}
-
-func gentraceback(pc0, sp0, lr0 uintptr, gp *g, skip int, pcbuf *uintptr, max int, callback func(*stkframe, unsafe.Pointer) bool, v unsafe.Pointer, flags uint) int {
-	return 0
-}
-
-func gcallers(gp *g, skip int, pcbuf []uintptr) int {
-	return 0
-}
-
-func goroutineheader(gp *g) {
-
-}
-
-func tracebackothers(me *g) {
-
-}
-
-func traceback(pc, sp, lr uintptr, gp *g) {
-
-}
-
-func tracebacktrap(pc, sp, lr uintptr, gp *g) {
-
 }
 
 var cgoSymbolizer unsafe.Pointer
