406ff765d43ee94a21fecc7d35a76e1ef7819547
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-03-23 14:26:42 -0400

    remove

--- src/utf8.go
+++ src/utf8.go
@@ -1,17 +1,1 @@
 package runtime
-
-const (
-	surrogateMin = 0xD800
-	surrogateMax = 0xDFFF
-)
-
-const runeError = '\uFFFD'
-
-func encoderune(p []byte, r rune) int {
-	return 0
-}
-
-// builtin
-func decoderune(s string, k int) (r rune, pos int) {
-	return
-}
