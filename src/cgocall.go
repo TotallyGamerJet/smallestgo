a003e483bc83d788f14754f6d3b8c6ad9d071b6e
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-03-23 14:23:07 -0400

    remove

--- src/cgocall.go
+++ src/cgocall.go
@@ -1,19 +1,1 @@
 package runtime
-
-import "unsafe"
-
-type cgoCallers [32]uintptr
-
-var ncgocall uint64 // number of cgo calls in total for dead m
-
-//go:nosplit
-//go:nowritebarrierrec
-func cgoIsGoPointer(p unsafe.Pointer) bool {
-	return false
-}
-
-func cgoInRange(p unsafe.Pointer, start, end uintptr) bool {
-	return false
-}
-
-func cgocall(fn, arg unsafe.Pointer) int32 { return 0 }
